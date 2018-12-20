19e5777878cb324c69743d9bd6416e1bd7e06e90
gianimpronta <gian.impronta@gmail.com>
2018-12-20 17:41:03 -0200

    Inicial

---
+++ project/leads/views.py
@@ -0,0 +1,8 @@
+from .serializers import LeadSerializer
+from .models import Lead
+from rest_framework import generics
+
+
+class LeadListCreate(generics.ListCreateAPIView):
+    queryset = Lead.objects.all()
+    serializer_class = LeadSerializer
